7cdb4229dcec56a18578d39dcb4ebfa551a79bb0
pankajjsdev <pankaj.js.dev@gmail.com>
2024-08-11 12:51:32 +0530

    start work on module, episodes

--- src/v1/db/index.js
+++ src/v1/db/index.js
@@ -5,13 +5,13 @@ const logger = require('../config/logger');
 // Get MongoDB URI from environment variable
 const uri = `${process.env.MONGODB_URI}/${process.env.DATABASE_NAME}`;
 
-console.log("connecting to db...", uri);
+console.log("connecting to db...");
 const connectDB = async () => {
   try {
     await mongoose.connect(uri);
-    console.log("Connected to MongoDB");
+    console.log("Connected to db");
   } catch (err) {
-    console.error("Error connecting to MongoDB:", err.message);
+    console.error("Error connecting to db:", err.message);
     logger.error(err)
     process.exit(1); // Exiting the process with code 1 upon connection failure
   }
